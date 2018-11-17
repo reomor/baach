1a7950956076dbad9bdb7eae5851eeac0fdda3af
Reomor <reomor@gmail.com>
2018-11-17 22:59:36 +0300

    liquibase insert

--- threesh/spring-application/src/main/java/com/reomor/MainApplication.java
+++ threesh/spring-application/src/main/java/com/reomor/MainApplication.java
@@ -1,12 +1,24 @@
 package com.reomor;
 
+import com.reomor.core.domain.UserRoles;
+import com.reomor.impl.service.UserAuthorizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.reomor")
-public class MainApplication {
+public class MainApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserAuthorizationService userService;
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        userService.register("user", "user@user.com", "123", UserRoles.ROLE_ADMIN);
     }
 }
