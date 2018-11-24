7771ceccef8ae977564a8e9e1e80b86056648641
Reomor <reomor@gmail.com>
2018-11-25 00:58:08 +0300

    fix security, main, hide properties

--- threesh/spring-application/src/main/java/com/reomor/MainApplication.java
+++ threesh/spring-application/src/main/java/com/reomor/MainApplication.java
@@ -1,24 +1,13 @@
 package com.reomor;
 
-import com.reomor.core.domain.UserRoles;
-import com.reomor.impl.service.UserAuthorizationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.reomor")
-public class MainApplication implements CommandLineRunner {
-
-    @Autowired
-    private UserAuthorizationService userService;
+public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        userService.register("user", "user@user.com", "123", UserRoles.ROLE_ADMIN);
-    }
 }
