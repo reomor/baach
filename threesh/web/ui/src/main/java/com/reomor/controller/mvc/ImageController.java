e03803a17d41dfb7b2e2e56e1943c904c4114382
Reomor <reomor@gmail.com>
2018-11-26 00:20:51 +0300

    image base controller and view, unite gallery support

---
+++ threesh/web/ui/src/main/java/com/reomor/controller/mvc/ImageController.java
@@ -0,0 +1,23 @@
+package com.reomor.controller.mvc;
+
+import com.reomor.core.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @Autowired
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @GetMapping("/image")
+    public String users(Model model) {
+        return "image/list";
+    }
+}
